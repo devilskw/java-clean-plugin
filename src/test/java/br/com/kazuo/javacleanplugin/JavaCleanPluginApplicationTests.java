61a092d1b39755503b2aafc54e4b5ddb8df273a3
devilskw <kazuo_w@hotmail.com>
2021-12-10 19:17:55 -0300

    feat(initial): Initial project

---
+++ src/test/java/br/com/kazuo/javacleanplugin/JavaCleanPluginApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kazuo.javacleanplugin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCleanPluginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
